bf3ce2dc2ea7365a48fd2a922c77bb5b010fe383
Alessandro <marzucoalessandro1@gmail.com>
2020-11-26 16:28:31 +0100

    funzione hide su i fa-times funzionante

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 //mostrare /nascondere menu principale con un click.
 //l'icona i fas fa-bars deve aprire il menu hamburger al click.
-$
 
-$("i.fa-bars").click(function () {
+$("i.fa-bars").click(function() {
   $("div.hamburger-menu").show();
+});
 
+//aperto il nostro hamburger-menu l'icona scompare e appare una nuova icona che ci servirà per chiudere questa volta hamburger-menu.
+$("i.fa-times").click(function() {
+  $("div.hamburger-menu").hide();
 });
