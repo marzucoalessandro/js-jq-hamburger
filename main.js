2ce31c1680451612e6f916163c48d8d6580cbdc6
Alessandro <marzucoalessandro1@gmail.com>
2020-11-26 16:37:12 +0100

    proviamo ad aggiungere il tempo per show e hide

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 //l'icona i fas fa-bars deve aprire il menu hamburger al click.
 
 $("i.fa-bars").click(function() {
-  $("div.hamburger-menu").show();
+  $("div.hamburger-menu").show(3000);
 });
 
 //aperto il nostro hamburger-menu l'icona scompare e appare una nuova icona che ci servirà per chiudere questa volta hamburger-menu.
 $("i.fa-times").click(function() {
-  $("div.hamburger-menu").hide();
+  $("div.hamburger-menu").hide(3000);
 });
