cf5292fff8ded57e5e641fd32f736fc184f5fc5e
Alessandro <marzucoalessandro1@gmail.com>
2020-11-26 15:53:22 +0100

    provata funzione show su  i non funzionante

---
+++ main.js
@@ -0,0 +1,8 @@
+//mostrare /nascondere menu principale con un click.
+//l'icona i fas fa-bars deve aprire il menu hamburger al click.
+$(i.fa-bars).addClass("littleMenu");
+
+$(.littleMenu).click(function ()) {
+  $(div.hamburger-menu).show();
+  
+}
